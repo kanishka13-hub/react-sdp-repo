e7f5e683f6b8872d7892d7575e80208a8321736f
2500032054@kluniversity.in <koppisettikanishka@gmail.com>
2026-03-03 11:50:59 +0530

    resolved merge conflict with u1

--- src/demo.js
+++ src/demo.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 console.log("I am in demo")
-console.log("hello")    
-console.log("bye..")
-=======
-console.log("I am in demo")
->>>>>>> parent of 73722e9 (modified demo.js by main)
+console.log("Hello....")    
+console.log("KLU")
